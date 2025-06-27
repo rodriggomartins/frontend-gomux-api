cce72cd63bf6f36be1beff20a38f8f978fe3cd13
rodriggomartins <rodriggodev@outlook.com>
2025-06-27 00:14:34 -0300

    mudei as cores ta tela de login, e arrumei o bug dos inputs

--- src/components/FormsComponent.tsx
+++ src/components/FormsComponent.tsx
@@ -2,8 +2,8 @@ import { Input } from "./Input"
 
 export const FormsComponent = () => {
   return (
-    <div className="text-center p-20 flex flex-col justify-center rounded-r-lg w-auto bg-tetest-400">
-      <h3 className="font-mypoppins text-2xl text-mygreen-600 font-bold">Login</h3>
+    <div className="text-center p-20 flex flex-col justify-center rounded-r-lg w-auto bg-myblue-600">
+      <h3 className="font-mypoppins text-2xl text-mywhite-500 font-bold">Login</h3>
       <form className="flex flex-col">
         <Input />
       </form>
