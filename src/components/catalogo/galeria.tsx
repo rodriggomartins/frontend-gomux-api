27fd1a8aae6fc22b9eefd519bcdf3856fefbbf6a
rodriggomartins <rodriggodev@outlook.com>
2025-07-05 21:39:09 -0300

    catalogo pegando forma

--- src/components/catalogo/galeria.tsx
+++ src/components/catalogo/galeria.tsx
@@ -1,13 +1,118 @@
 export const Galeria = () => {
   return(
-    <div className="h-auto w-auto flex space-x-4 mt-1">
-      {/*
-      <div className="h-100 w-100 bg-red-600">
-        oi
+    <div className="h-auto w-screen flex space-x-4 p-2
+      bg-white-r-600">
+      <div className="h-auto w-80 border border-myborder-600 rounded-sm">
+        <img src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D" alt="produtos-one"  className="rounded-sm p-2"/>
+        <div className="flex items-center flex-col">
+          <span className="text-myblack-600 font-medium">Price: 5.000</span>
+          <span className="text-myblack-600 font-medium">SmartWatch</span>
+          <p className="text-myblack-600 font-medium cursor-pointer">Detalhes</p>
+          <div>
+          <button className="bg-mygreen-600 text-center m-3 p-2 text-mywhite-500 rounded-sm cursor-pointer border border-mywhite-500 w-30 hover:bg-sky-700">
+              Comprar
+            </button>
+          </div>
+        </div>
       </div>
-      <div className="h-100 w-100 bg-red-600">
-        oi
-      </div>*/}
+      <div className="h-auto w-80 border border-myborder-600 rounded-sm">
+        <img src="https://images.unsplash.com/photo-1549482199-bc1ca6f58502?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OTh8fHByb2R1Y3R8ZW58MHx8MHx8fDA%3D" alt="produtos-one"  className="rounded-sm p-2"/>
+        <div className="flex items-center flex-col">
+          <span className="text-myblack-600 font-medium">Price: 5.000</span>
+          <span className="text-myblack-600 font-medium">SmartWatch</span>
+          <p className="text-myblack-600 font-medium cursor-pointer">Detalhes</p>
+          <div>
+          <button className="bg-mygreen-600 text-center m-3 p-2 text-mywhite-500 rounded-sm cursor-pointer border border-mywhite-500 w-30 hover:bg-sky-700">
+              Comprar
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div className="h-auto w-80 border border-myborder-600 rounded-sm">
+        <img src="https://images.unsplash.com/flagged/photo-1556637640-2c80d3201be8?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nzh8fHByb2R1Y3R8ZW58MHx8MHx8fDA%3D" alt="produtos-one"  className="rounded-sm p-2"/>
+        <div className="flex items-center flex-col">
+          <span className="text-myblack-600 font-medium">Price: 5.000</span>
+          <span className="text-myblack-600 font-medium">SmartWatch</span>
+          <p className="text-myblack-600 font-medium cursor-pointer">Detalhes</p>
+          <div>
+          <button className="bg-mygreen-600 text-center m-3 p-2 text-mywhite-500 rounded-sm cursor-pointer border border-mywhite-500 w-30 hover:bg-sky-700">
+              Comprar
+            </button>
+          </div>
+        </div>
+      </div>
+
+
+      <div className="h-auto w-80 border border-myborder-600 rounded-sm">
+        <img src="https://images.unsplash.com/photo-1491637639811-60e2756cc1c7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjN8fHByb2R1Y3R8ZW58MHx8MHx8fDA%3D" alt="produtos-one"  className="rounded-sm p-2"/>
+        <div className="flex items-center flex-col">
+          <span className="text-myblack-600 font-medium">Price: 5.000</span>
+          <span className="text-myblack-600 font-medium">SmartWatch</span>
+          <p className="text-myblack-600 font-medium cursor-pointer">Detalhes</p>
+          <div>
+          <button className="bg-mygreen-600 text-center m-3 p-2 text-mywhite-500 rounded-sm cursor-pointer border border-mywhite-500 w-30 hover:bg-sky-700">
+              Comprar
+            </button>
+          </div>
+        </div>
+      </div>
+
+
+
+      <div className="h-auto w-80 border border-myborder-600 rounded-sm">
+        <img src="https://images.unsplash.com/photo-1583394838336-acd977736f90?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjR8fHByb2R1Y3R8ZW58MHx8MHx8fDA%3D" alt="produtos-one"  className="rounded-sm p-2"/>
+        <div className="flex items-center flex-col">
+          <span className="text-myblack-600 font-medium">Price: 5.000</span>
+          <span className="text-myblack-600 font-medium">SmartWatch</span>
+          <p className="text-myblack-600 font-medium cursor-pointer">Detalhes</p>
+          <div>
+          <button className="bg-mygreen-600 text-center m-3 p-2 text-mywhite-500 rounded-sm cursor-pointer border border-mywhite-500 w-30 hover:bg-sky-700">
+              Comprar
+            </button>
+          </div>
+        </div>
+      </div>
+
+
+
+
+      <div className="h-auto w-80 border border-myborder-600 rounded-sm">
+        <img src="https://plus.unsplash.com/premium_photo-1661597156656-75ba116e9e1d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjF8fHByb2R1Y3R8ZW58MHx8MHx8fDA%3D" alt="produtos-one"  className="rounded-sm p-2"/>
+        <div className="flex items-center flex-col">
+          <span className="text-myblack-600 font-medium">Price: 5.000</span>
+          <span className="text-myblack-600 font-medium">SmartWatch</span>
+          <p className="text-myblack-600 font-medium cursor-pointer">Detalhes</p>
+          <div>
+          <button className="bg-mygreen-600 text-center m-3 p-2 text-mywhite-500 rounded-sm cursor-pointer border border-mywhite-500 w-30 hover:bg-sky-700">
+              Comprar
+            </button>
+          </div>
+        </div>
+      </div>
+
+
+
+      <div className="h-auto w-80 border border-myborder-600 rounded-sm">
+        <img src="https://images.unsplash.com/photo-1602143407151-7111542de6e8?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D" alt="produtos-one"  className="rounded-sm p-2"/>
+        <div className="flex items-center flex-col">
+          <span className="text-myblack-600 font-medium">Price: 5.000</span>
+          <span className="text-myblack-600 font-medium">SmartWatch</span>
+          <p className="text-myblack-600 font-medium cursor-pointer">Detalhes</p>
+          <div>
+          <button className="bg-mygreen-600 text-center m-3 p-2 text-mywhite-500 rounded-sm cursor-pointer border border-mywhite-500 w-30 hover:bg-sky-700">
+              Comprar
+            </button>
+          </div>
+        </div>
+      </div>
+
+
+
+
+
+
+
 
     </div>
   )
